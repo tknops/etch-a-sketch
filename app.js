24ed104cac4c4d8baf90d9c8c8fb377c6a4b9a96
Tim Knops <timknops22@gmail.com>
2022-10-12 22:54:58 +0200

    Hover effect and slider that reacts with grid

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-const grid = document.querySelector('#grid');
+const grid = document.getElementById('grid');
+const slider = document.getElementById('myRange');
 
+function createGrid(newValue) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
 
+    let i = 1;
+    for (i; i <= newValue; i++) {
+        grid.appendChild(gridItem.cloneNode(true));
+    }
 
-const gridItem = document.createElement('div');
-gridItem.classList.add('grid-item');
+    let gridNum = Math.sqrt(i-1);
+    grid.setAttribute (
+        'style',
+        `grid-template-columns: repeat(${gridNum}, 1fr);`
+    );
 
-let i = 1;
-for (i; i <= 16; i++) {
-    grid.appendChild(gridItem.cloneNode(true));
+    hoverOverGrid();
+    grid.append();
 }
 
-let gridNum = Math.sqrt(i-1);
-grid.setAttribute (
-    'style',
-    `grid-template-columns: repeat(${gridNum}, 1fr);`
-);
+function hoverOverGrid() {
+    const gridItems = grid.childNodes;
 
-grid.append();
-console.log(gridNum);
+    gridItems.forEach((e) => {
+        e.addEventListener('mouseover', () => {
+            e.classList.add('grid-item-dark-bg');
+        });
+    });
+}
+
+function sliderAmount() {
+    slider.oninput = () => {
+        let newValue = slider.value * slider.value;
+        grid.innerHTML = '';
+        createGrid(newValue);
+    }
+}
+
+sliderAmount();
+createGrid(16);
